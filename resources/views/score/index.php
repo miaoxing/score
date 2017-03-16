cc94075fb9a0f6c1c8e8afe488ffd0c0c33b3a2b
twinh <twinhuang@qq.com>
2017-03-16 21:39:00 +0800

    移除多余的样式 closes #3221 & closes #3222 & closes #3223 & closes #3224  & closes #3231 & closes #3232 & closes #3233

--- resources/views/score/index.php
+++ resources/views/score/index.php
@@ -1,7 +1,5 @@
 <?php $view->layout('plugin:layouts/jqm.php') ?>
 
-<?php require $view->getFile('@order/mall/asset-jqm.php') ?>
-
 <?= $block('css') ?>
 <link rel="stylesheet" href="<?= $asset('plugins/score/css/score.css') ?>"/>
 <?= $block->end() ?>
