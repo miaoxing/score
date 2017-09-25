37da06b5a63e6b2162153124b4c8011ab42517b4
twinh <twinhuang@qq.com>
2017-09-25 16:16:18 +0800

    移除多余action closes #3768 closes #3769 closes #3770 closes #3771

--- src/Controller/Score.php
+++ src/Controller/Score.php
@@ -4,15 +4,6 @@ namespace Miaoxing\Score\Controller;
 
 class Score extends \miaoxing\plugin\BaseController
 {
-    public function indexAction()
-    {
-        $headerTitle = $this->setting('score.title', '积分') . '列表';
-        $scoreList = wei()->score->getScoreLog($this->curUser['id']);
-        $userInfo = wei()->user()->where('id=?', $this->curUser['id'])->fetch();
-
-        return get_defined_vars();
-    }
-
     public function getUserScoreAction($req)
     {
         $score = wei()->score->getScore();
