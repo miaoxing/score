e7b8b0a81b208af9eae793e65ba7f0f2c7be6a45
twinh <twinhuang@qq.com>
2018-11-01 14:55:27 +0800

    fix: 用户积分来源可能为负数

---
+++ src/Migration/V20181101145341UpdateUserSourceScoresTable.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Miaoxing\Score\Migration;
+
+use Miaoxing\Plugin\BaseMigration;
+
+class V20181101145341UpdateUserSourceScoresTable extends BaseMigration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function up()
+    {
+        $this->schema->table('user_source_scores')
+            ->int('score')->unsigned(false)->change()
+            ->exec();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function down()
+    {
+        $this->schema->table('user_source_scores')
+            ->int('score')->unsigned(true)->change()
+            ->exec();
+    }
+}
