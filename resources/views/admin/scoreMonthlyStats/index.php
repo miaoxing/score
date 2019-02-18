5ff228b2db4e15427b3488af45f124df64ded1ed
twinh <twinhuang@qq.com>
2019-02-18 21:08:43 +0800

    refactor: 移除/更新一些工具类

--- resources/views/admin/scoreMonthlyStats/index.php
+++ resources/views/admin/scoreMonthlyStats/index.php
@@ -33,7 +33,7 @@ $view->layout();
         <a class="nav-link" href="#score" aria-controls="score" role="tab" data-toggle="tab">积分变动</a>
       </li>
     </ul>
-    <div class="tab-content m-t no-border">
+    <div class="tab-content m-t border-0">
       <div role="tabpanel" class="js-chart-pane tab-pane text-center active" id="score">
         加载中...
       </div>
